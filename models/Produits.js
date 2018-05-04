3bca63ebeece94ca010429376c98d3ab3ceabd7e
Tommy Laurent <tommylaurent@protonmail.com>
2018-05-04 11:41:18 +0200

    ajout de la liste

--- models/Produits.js
+++ models/Produits.js
@@ -1,6 +1,10 @@
 const Produits = (sequelize, DataTypes) => {
   var Produits = sequelize.define("Produits", {
+    //id: { type: DataTypes.INT },
     titre: { type: DataTypes.STRING }
+    //image: { type: DataTypes.STING },
+    //note: { type: DataTypes.INT }
+    //reference: { type: DataTypes.STRING, is: ["[A-Z-]"] }
   });
 
   return Produits;
