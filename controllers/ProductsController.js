8b06e5b6265a26875b12a32460fd23c9f3278dfe
Julien Foucher <blomki@gmail.com>
2018-05-04 12:50:01 +0200

    ajout du formulaire : perte de temps de ouf sur l'ordre des routes

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -3,6 +3,7 @@ const sequelize = require(`sequelize`);
 
 class ProductsController {
   viewProduct(req, res) {
+    console.log("fonction view");
     db.Products.findById(req.params.id).then(product => {
       //  ... sending to the view
       res.render("pages/product", {
@@ -11,15 +12,21 @@ class ProductsController {
       });
     });
   }
+  displayAddForm(req, res) {
+    //console.log("ok");
+    res.render("pages/formCreateProduct");
+  }
   productsListing(req, res) {
+    console.log("fonction listing");
     db.Products.findAll({ order: sequelize.literal("(score) DESC") }).then(
       products => {
-        //  ... sending to the view
-        res.render("pages/products", {
-          // we send the results
-          products
-        });
+        res.render("pages/products", { products });
       }
+    );
+  }
+  saveProduct(req, res) {
+    db.Articles.create(req.body).then(produit =>
+      res.redirect(`/produits/${req.body.id}`)
     );
   }
 }
--- routes/products.js
+++ routes/products.js
@@ -7,30 +7,10 @@ const router = express.Router();
 const ProductsController = require("../controllers/ProductsController");
 const controller = new ProductsController();
 
-router.get("/produits", (req, res) => controller.productsListing(req, res));
+router.get("/produit/ajouter", (req, res) =>
+  controller.displayAddForm(req, res)
+);
 router.get("/produit/:id", (req, res) => controller.viewProduct(req, res));
-
-// /**
-//  * Middleware vérifier si je suis connecté
-//  */
-// function loggedIn(req, res, next) {
-//   if (req.user) {
-//     // si l'utilisateur est connecté
-//     // if request contains the user
-//     next(); // call next
-//   } else {
-//     return res.redirect("/auth/login"); // throwing unauthorized
-//   }
-// }
-
-// function isAnonymous(req, res, next) {
-//   if (!req.user) {
-//     // if request contains the user
-//     next(); // call next
-//   } else {
-//     return res.redirect("/private"); // throwing unauthorized
-//   }
-// }
-// router.get("/private", loggedIn, (req, res) => controller.private(req, res));
+router.get("/produits", (req, res) => controller.productsListing(req, res));
 
 module.exports = router;
